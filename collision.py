b68aee623f4e88c34587c4296b6aae0cd65f89cb
Andy Pellett <anrope@gmail.com>
2013-06-01 17:51:13 -0400

    backend server with search by town

--- collision.py
+++ collision.py
@@ -1,13 +1,35 @@
 import tornado.ioloop
 import tornado.web
 import riak
+import uuid
+import urlparse
+import json
+
+rk = riak.RiakClient()
+
+rkb = rk.bucket('collisions')
 
 class ReportHandler(tornado.web.RequestHandler):
     def get(self):
-        print 'getting accident report'
+        query = urlparse.parse_qs(self.request.query)
+        for k, v in query.items():
+            if k not in ['town', 'county', 'notes']:
+                self.send_error(status_code=403)
+                self.finish()
+                return
+
+        town = query['town'][0]
+        accident_search = rk.search('collisions', 'town:{}'.format(town)).run()
+
+        self.write(json.dumps([a.get().get_data() for a in accident_search]))
+        self.finish()
 
     def post(self):
-        print 'received new accident report'
+        accident_id = uuid.uuid4().hex
+        accident = json.loads(self.request.body)
+        acc = rkb.new(accident_id, data=accident)
+        acc.store()
+
 
 application = tornado.web.Application([
     (r"/1/report", ReportHandler),
