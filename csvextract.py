c8d412aa4f73a37eb2438303cc4de449e0a8d28a
Andy Pellett <anrope@gmail.com>
2013-06-02 13:21:35 -0400

    add time to csv extracted reports

--- csvextract.py
+++ csvextract.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 incsv = open('../crashdata.csv', 'r')
 
@@ -15,8 +16,8 @@ for line in incsv:
     accident = {
         'latitude': l[1],
         'longitude': l[2],
-        'location_start': l[6],
-        'location_end': l[8],
+        'location_start': l[6].replace('"', '').strip(),
+        'location_end': l[8].replace('"', '').strip(),
         'county': '',
         'town': l[0],
         'notes': '',
@@ -24,9 +25,9 @@ for line in incsv:
         'county_rank': l[-1],
         'total_crashes': l[-4],
         'crf': l[-3],
+        'time': datetime.datetime(year=2009, month=1, day=1).strftime(
+            '%Y-%m-%dT%H:%M:%S')
     }
 
-    requests.post('http://localhost:14441/api/1/report', data=json.dumps(accident))
-    #requests.post('http://evilteam.com/api/1/report', data=json.dumps(accident))
-
-    #print accident['latitude'], accident['longitude'], accident['crf'], accident['county_rank'], accident['state_rank'], accident['town']
+    #requests.post('http://localhost:14441/api/1/report', data=json.dumps(accident))
+    requests.post('http://evilteam.com/api/1/report', data=json.dumps(accident))
